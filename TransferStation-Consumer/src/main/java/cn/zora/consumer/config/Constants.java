b9477efdc0bcf03bb5a9a5e5db9394adc55e6a7d
ZoraGK <zora759gk@gmail.com>
2020-04-28 11:00:02 +0800

    Update Constants.java

--- TransferStation-Consumer/src/main/java/cn/zora/consumer/config/Constants.java
+++ TransferStation-Consumer/src/main/java/cn/zora/consumer/config/Constants.java
@@ -9,5 +9,5 @@ package cn.zora.consumer.config;
  * @since 2020.04.27
  */
 public class Constants {
-    public static final String STATION_PREFIX ="http://106.52.180.188:6169/pusher";
+    public static final String STATION_PREFIX ="http://your_host:your_port/pusher";
 }
